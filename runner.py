b1d977f5d6e8a35f5caf9465c07db12e1504ddfa
Saffron Liu <64503620+SapphireHue@users.noreply.github.com>
2024-12-18 15:19:36 -0600

    Simple parsing script

--- runner.py
+++ runner.py
@@ -1,13 +1,21 @@
+"""Simple script to extract and parse PDF packets."""
+
 import pdf_to_text
 import parse_packet
 import os
 
-INPUT_DIRECTORY = "packets\\input"
-OUTPUT_DIRECTORY_1 = "packets\\extracted"
-OUTPUT_DIRECTORY_2 = "packets\\parsed"
+INPUT_PDFS = "packets\\input"
+EXTRACTED_TEXT = "packets\\extracted"
+PARSED_JSON = "packets\\parsed"
 
-files = os.listdir(INPUT_DIRECTORY)
-
+# extract text
+files = os.listdir(INPUT_PDFS)
 for file in files:
-    pdf_to_text.extractText(file, INPUT_DIRECTORY, OUTPUT_DIRECTORY_1)
-    parse_packet.parseText(file[:-4]+".txt", OUTPUT_DIRECTORY_1, OUTPUT_DIRECTORY_2)
+    pdf_to_text.extractText(file, INPUT_PDFS, EXTRACTED_TEXT)
+    os.rename(os.path.join(INPUT_PDFS, file), os.path.join("packets\\used\\pdfs", file)) # move to used pdfs
+
+# parse packets
+files = os.listdir(EXTRACTED_TEXT)
+for file in files:
+    parse_packet.parseText(file, EXTRACTED_TEXT, PARSED_JSON)
+    os.rename(os.path.join(EXTRACTED_TEXT, file), os.path.join("packets\\used\\txts", file)) # move to used txts
